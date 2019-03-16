1cf9f6d7c1f27d451ceed8b4a0c568b9f3e1ae3e
aliyev12 <aaa719@nyu.edu>
2019-03-16 18:48:40 -0400

    add signup, signin and logout features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const cookieParser = require('cookie-parser');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -8,7 +8,20 @@ const server = createServer();
 
 // TODO Use express middleware to handle cookies (JWT)
 // Allow using any existing express middleware
-server.express.use(cookieParset());
+server.express.use(cookieParser());
+
+// Decode the JWT so we can get the user ID on each erquest
+server.express.use((req, res, next) => {
+    const {token} = req.cookies;
+    if(token) {
+        const {userId} = jwt.verify(token, process.env.APP_SECRET);
+        // Put the userId onto the req for future requests to access
+        req.userId = userId;
+    }
+    next();
+});
+
+
 // TODO Use express middleware to populate current user
 
 server.start({
@@ -22,4 +35,12 @@ server.start({
 
 /*
 Middleware is a function that runs in the middle of request and response
+*/
+
+/*
+Creating queries/mutations:
+
+1. Add it to the schema;
+2. Add a resolver whether its a Mutation or a Query
+3. Build an interface in the front end
 */
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,8 +1,22 @@
-const bcrypt = require('bcryptjs')
-    jwt = require('jsonwebtoken');
+const bcrypt = require ('bcryptjs');
+jwt = require ('jsonwebtoken');
 
+// This function will create/sign a JWT token, and it will attache a cookie with that token to response
+const signCookie = (ctx, userId) => {
+  // Generage the JWT token
+  const token = jwt.sign ({userId}, process.env.APP_SECRET);
+  // We set the JWT as a cookie on the response so every time that user clicks on another page, the token comes on the ride
+  return ctx.response.cookie ('token', token, {
+    // Make sure that its HTTP only so that a third party cannot get it with JavaScript, or some rogue browser extension etc.
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 10,
+  });
+};
 
 const Mutations = {
+  /*===================*/
+  /*=== CREATE ITEM ===*/
+  /*===================*/
   //   createItem(data: ItemCreateInput!): Item!
   async createItem (parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -10,6 +24,10 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem ({data: {...args}}, info);
     return item;
   },
+
+  /*===================*/
+  /*=== UPDATE ITEM ===*/
+  /*===================*/
   //   updateItem(data: ItemUpdateInput!, where: ItemWhereUniqueInput!): Item
   updateItem (parent, args, ctx, info) {
     // First take a copy of the updates
@@ -27,6 +45,10 @@ const Mutations = {
       info
     );
   },
+
+  /*===================*/
+  /*=== DELETE ITEM ===*/
+  /*===================*/
   async deleteItem (parent, args, ctx, info) {
     const where = {id: args.id};
     // 1. Find the item
@@ -36,36 +58,66 @@ const Mutations = {
     // 3. Delete it!
     return ctx.db.mutation.deleteItem ({where}, info);
   },
-  async signup(parent, args, ctx, info) {
-      // Lowercase user email
-      args.email = args.email.toLowerCase();
-      // Hash user password and request salt with 10 characters
-    const password = await bcrypt.hash(args.password, 10);
+
+  /*===================*/
+  /*=== SIGN UP ===*/
+  /*===================*/
+  async signup (parent, args, ctx, info) {
+    const {email, password, name} = args;
+    if (!email || !password || !name)
+      throw new Error ('Please, provide all the required information');
+    // Lowercase user email
+    args.email = args.email.toLowerCase ();
+    // Hash user password and request salt with 10 characters
+    const hashedPassword = await bcrypt.hash (args.password, 10);
     // Create user in the database
-    const user = await ctx.db.mutation.createUser({
+    const user = await ctx.db.mutation.createUser (
+      {
         data: {
-            ...args,
-            password,
-            permissions: { set: ['USER'] }
-        }
-    }, info);
-    // Create the JWT token for user
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-    // We set the JWT as a cookie on the response so every time that click on another page the token comes on the ride
-    ctx.response.cookie('token', token, {
-        // Make sure that its HTTP only so that a third party cannot get it with JavaScript, or some rogue browser extension etc.
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 10 // This will set timeout for 10 day. You can add * 365 for it to be a year
-    });
+          ...args,
+          password: hashedPassword,
+          permissions: {set: ['USER']},
+        },
+      },
+      info
+    );
+    // Generate JWT token and attach cookie to response
+    signCookie (ctx, user.id);
     // Finally, we return the user to the browser
     return user;
-  } 
+  },
+
+  /*===================*/
+  /*=== SIGN IN ===*/
+  /*===================*/
+  async signin (parents, {email, password}, ctx, info) {
+    // 1. Check if email and password are truthy
+    if (!email || !password)
+      throw new Error ('Please, provide all the required information');
+    // 2. Check if there is a user with that email
+    const user = await ctx.db.query.user ({where: {email: email}});
+    if (!user) throw new Error (`No such user found for email ${email}`);
+    // 3. Check if user password is corrent
+    const valid = await bcrypt.compare (password, user.password);
+    if (!valid) throw new Error ('Invalid Password');
+    // Generate JWT token and attach cookie to response
+    signCookie (ctx, user.id);
+    // 6. Return the user
+    return user;
+  },
+
+  /*===================*/
+  /*=== SIGN OUT ===*/
+  /*===================*/
+  signout (parents, args, ctx, info) {
+      ctx.response.clearCookie('token');
+      return { message: 'Goodbye!' };
+  },
 };
 
 module.exports = Mutations;
 
-
-  /*
+/*
   When we create a user, we are setting a cookie with a currently logged in user.
   Every single time someone requests a page, the cookie is going to send
   along a JWT, and it will be kinda similar to sessions. It will send along
@@ -79,4 +131,26 @@ module.exports = Mutations;
   localStorage doesn't automatically send that token along. So there will be a slight delay before we pull the token and send it,
   and the first second or more the site will appear as if user is logged out. With cookies
   JWTs are sent automatically with every request so that we can immediately server render an authenticated user.
+  */
+
+/*
+  Useless code, delete eventually...
+
+
+      // // 4. Generage the JWT token
+    // const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // // 5. Set the cookie with token
+    // ctx.response.cookie('token', token, {
+    //     httpOnly: true,
+    //     maxAge: 1000 * 60 * 60 * 24 * 10
+    // });
+
+
+        // const token = jwt.sign ({userId: user.id}, process.env.APP_SECRET);
+    // // We set the JWT as a cookie on the response so every time that click on another page the token comes on the ride
+    // ctx.response.cookie ('token', token, {
+    //   // Make sure that its HTTP only so that a third party cannot get it with JavaScript, or some rogue browser extension etc.
+    //   httpOnly: true,
+    //   maxAge: 1000 * 60 * 60 * 24 * 10, // This will set timeout for 10 day. You can add * 365 for it to be a year
+    // });
   */
